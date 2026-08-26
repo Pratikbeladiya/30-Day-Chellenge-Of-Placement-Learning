a9b18002dffd8bf4a81e9ddf7be5f92d8770d315
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-08-26 21:51:56 +0530

    add the ES5 and ES6 understanding

---
+++ index.js
@@ -0,0 +1,22 @@
+
+console.log("welcome to the day 5 of Linkedin challenge");
+
+//Javascript made with 2 version: ES5(ECMAScript5) and ES6
+//in ES5 has a var variable
+//in ES6 has a let and const variable
+
+function ES5 (){
+    for(var i =0;i<12;i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+ES5();
+
+function ES5 (){
+    for(let i=0;i<12;i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+ES5();
