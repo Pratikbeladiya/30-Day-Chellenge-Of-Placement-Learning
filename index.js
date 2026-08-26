77a0e551f2961dbd1897264a91b31dd9a6cfd3d2
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-08-26 22:09:39 +0530

    add the ES6 logic and run the code

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ console.log("welcome to the day 5 of Linkedin challenge");
 function ES5 (){ //behave as a function scope
     console.log("this is ES5");
     for (var i =1;i<12;i++){
-        console.log("hello to the day 5");
+        console.log(i);
     }
-    console.log("next day");
+    console.log(i);
 }
+ES5();
+
+function ES6 (){ //behave as a block scope
+    console.log("this is ES6");
+    for (let i =1;i<12;i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+ES6();
