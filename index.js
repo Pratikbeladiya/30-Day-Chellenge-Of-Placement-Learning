a9e495776236c67d9ef49db2af36418243c4a2cd
Pratik Beladiya <beladiyapratik03@gmail.com>
2026-08-26 22:05:44 +0530

    add the ES5 logic

--- index.js
+++ index.js
@@ -5,18 +5,10 @@ console.log("welcome to the day 5 of Linkedin challenge");
 //in ES5 has a var variable
 //in ES6 has a let and const variable
 
-function ES5 (){
-    for(var i =0;i<12;i++){
-        console.log(i);
+function ES5 (){ //behave as a function scope
+    console.log("this is ES5");
+    for (var i =1;i<12;i++){
+        console.log("hello to the day 5");
     }
-    console.log(i);
+    console.log("next day");
 }
-ES5();
-
-function ES5 (){
-    for(let i=0;i<12;i++){
-        console.log(i);
-    }
-    console.log(i);
-}
-ES5();
